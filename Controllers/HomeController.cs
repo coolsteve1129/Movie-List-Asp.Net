ac91754ed707490fc4b0c8debb6acb1b1d00e9f2
Steven Heck <16365660@witc.edu>
2020-10-10 21:41:54 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using HeckAsg8Movie.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace HeckAsg8Movie.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MovieContext context { get; set; }
+        public HomeController(MovieContext ctx)
+        {
+            context = ctx;
+        }
+        public IActionResult Index()
+        {
+            var movies = context.Movies.Include(m => m.Genre).OrderBy(m => m.Name).ToList();
+
+            
+                return View(movies);
+            
+        }
+    }
+}
+
---
+++ Startup.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using HeckAsg8Movie.Models;
+namespace HeckAsg8Movie
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddDbContext<MovieContext>(options => options.UseSqlServer(
+                                                Configuration.GetConnectionString("MovieContext")));
+            services.AddRouting(options =>{
+                options.LowercaseUrls = true; options.AppendTrailingSlash = true;
+            }); 
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}/{slug?}");
+            });
+        }
+    }
+}
